1d5a85d9e6078b15e04622eb3d4118f6e577fc5c
ayaosamaramadan <ayaramadan2011@gmail.com>
2025-03-18 22:15:43 +0200

    add Navbar component ,update layout

---
+++ components/Navbar.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+export default function Navbar() {
+  return (
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-white text-lg font-bold">MyApp</div>
+        <ul className="flex space-x-4">
+          <li>
+              <Link href="/" className="text-white hover:text-gray-400">Home</Link>
+          </li>
+         
+        </ul>
+      </div>
+    </nav>
+  );
+}
